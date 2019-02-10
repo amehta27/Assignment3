7ff6912e7c28f79a76b97d11c2d1b0a3b1845825
amehta27 <apmk_27@yahoo.com>
2019-02-10 00:32:28 -0600

    initial commit

---
+++ styles/main1.js
@@ -0,0 +1,13 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+Github 
+username : amehta27
+pass : shreeji1973
